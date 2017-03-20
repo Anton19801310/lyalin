49f2a984b5aae2ba66e878d3aaed7a2cad38662e
Anton19801310 <rgn78@yandex.ru>
2017-03-20 21:13:28 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,12 @@
 package ru.job4j;
 
+/**
+*Class Calculate вывод на консоль строки "Hello world.".
+*@autor Lyalin
+*@since 20.03.2017
+*@version 1.0
+*/ 
+
 public class Calculate {
 	public static void main (String[] args) {
 		System.out.println("Hello world.");
---
+++ chapter_001/src/main/java/ru/job4j/Package-info.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+/**
+* Class Calculate вывод на консоль строки "Hello world.".
+* @autor Lyalin Anton (mailto:rgn78@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
